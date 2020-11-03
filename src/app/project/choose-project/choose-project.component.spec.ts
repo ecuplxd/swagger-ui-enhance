d15fafa417d386c2dc6d187a5c88994c9d89d63f
ecuplxd <12050901053@ecupl.edu.cn>
2020-11-03 11:00:50 +0800

    chore(test): fix test error

--- src/app/project/choose-project/choose-project.component.spec.ts
+++ src/app/project/choose-project/choose-project.component.spec.ts
@@ -1,5 +1,6 @@
 import { NO_ERRORS_SCHEMA } from '@angular/compiler';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatMenuModule } from '@angular/material/menu';
 import { ChooseProjectComponent } from './choose-project.component';
 
 describe('ChooseProjectComponent', () => {
@@ -9,6 +10,7 @@ describe('ChooseProjectComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       schemas: [NO_ERRORS_SCHEMA],
+      imports: [MatMenuModule],
       declarations: [ChooseProjectComponent],
     }).compileComponents();
   });
