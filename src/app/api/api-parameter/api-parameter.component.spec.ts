34682282314cc26a5ae7836d0ebefd9983d4245a
ecuplxd <12050901053@ecupl.edu.cn>
2020-11-09 01:07:57 +0800

    chore(test): ApiParameterComponent

--- src/app/api/api-parameter/api-parameter.component.spec.ts
+++ src/app/api/api-parameter/api-parameter.component.spec.ts
@@ -1,12 +1,9 @@
 import { NO_ERRORS_SCHEMA } from '@angular/compiler';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { IdService } from 'src/app/share/service';
+import { storeDataMock } from 'src/__test__';
 import { ApiModule } from '../api.module';
 import { ApiParameterComponent } from './api-parameter.component';
-
-class IdServiceStub {
-  genID(): void {}
-}
 
 describe('ApiParameterComponent', () => {
   let component: ApiParameterComponent;
@@ -17,12 +14,7 @@ describe('ApiParameterComponent', () => {
       schemas: [NO_ERRORS_SCHEMA],
       imports: [ApiModule],
       declarations: [ApiParameterComponent],
-      providers: [
-        {
-          provide: IdService,
-          useClass: IdServiceStub,
-        },
-      ],
+      providers: [IdService],
     }).compileComponents();
   });
 
@@ -34,5 +26,76 @@ describe('ApiParameterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(component.copyItemClass).toBeTruthy('init create a copy id');
+    expect(component.hadParameters).toBe(false, 'init not had parameters');
+  });
+
+  it('should show no parameters tip if #hadParameters=false', () => {
+    const tr: HTMLTableRowElement = fixture.nativeElement.querySelector(
+      '.no-data'
+    );
+    const div: HTMLDivElement = fixture.nativeElement.querySelector(
+      '.description'
+    );
+    const copy = fixture.nativeElement.querySelector(
+      '.parameter-fields app-copy'
+    );
+
+    expect(tr).toBeTruthy('show no parameters el');
+    expect(div).toBeNull('no parameters to show');
+    expect(copy).toBeNull(
+      'no parameters, not show copy all parameter fields el'
+    );
+  });
+
+  it('should show api parameters table if #hadParameters=true', () => {
+    fixture = TestBed.createComponent(ApiParameterComponent);
+    component = fixture.componentInstance;
+    component.parameters = storeDataMock.apiItems[7].parameters;
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    expect(component.hadParameters).toBe(true);
+
+    const tr: HTMLTableRowElement = fixture.nativeElement.querySelector(
+      '.no-data'
+    );
+    const copy = fixture.nativeElement.querySelector(
+      '.parameter-fields app-copy'
+    );
+    const parameterItems: HTMLElement[] = fixture.nativeElement.querySelectorAll(
+      '.parameter-item'
+    );
+    const parameterTypes: HTMLElement[] = fixture.nativeElement.querySelectorAll(
+      'app-api-type'
+    );
+    const descriptions: HTMLElement[] = fixture.nativeElement.querySelectorAll(
+      '.description'
+    );
+    const parameterIns: HTMLElement[] = fixture.nativeElement.querySelectorAll(
+      '.parameter-in'
+    );
+
+    expect(component.hadParameters).toBe(true, 'api have parameters');
+    expect(tr).toBeNull('hvae parameters, not show no parameters tip el');
+    expect(copy).toBeTruthy(
+      'hvae parameters, show copy all parameter fields el'
+    );
+    expect(parameterItems.length).toEqual(component.parameters.length);
+    expect(parameterTypes.length).toEqual(component.parameters.length);
+    expect(descriptions.length).toEqual(component.parameters.length);
+    expect(parameterIns.length).toEqual(component.parameters.length);
+
+    parameterTypes.forEach((item) =>
+      expect(item).toBeTruthy('had parameter type el')
+    );
+    parameterIns.forEach((item, index) =>
+      expect(item.innerText).toEqual('(' + component.parameters[index].in + ')')
+    );
+    descriptions.forEach((item, index) =>
+      expect(item.innerText).toEqual(
+        component.parameters[index].description as string
+      )
+    );
   });
 });
