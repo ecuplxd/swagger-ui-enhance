29d16a5bc4c8c9d5a5366e24ffb968131a967a48
ecuplxd <12050901053@ecupl.edu.cn>
2020-11-18 20:57:49 +0800

    chore(test): MenuPositionService

--- src/app/share/service/menu-position/menu-position.service.spec.ts
+++ src/app/share/service/menu-position/menu-position.service.spec.ts
@@ -1,6 +1,82 @@
+import { ConnectedPosition } from '@angular/cdk/overlay';
 import { TestBed } from '@angular/core/testing';
 
 import { MenuPositionService } from './menu-position.service';
+import { MenuPositionValue } from './menu.model';
+
+interface TestPosition {
+  side: MenuPositionValue;
+  align: MenuPositionValue;
+  positions: ConnectedPosition[];
+}
+
+const POSITIONS: TestPosition[] = [
+  {
+    side: 'top',
+    align: 'left',
+    positions: [
+      {
+        originX: 'start',
+        originY: 'top',
+        overlayX: 'start',
+        overlayY: 'bottom',
+        offsetY: 0,
+      },
+    ],
+  },
+  {
+    side: 'right',
+    align: 'top',
+    positions: [
+      {
+        originX: 'end',
+        originY: 'top',
+        overlayX: 'start',
+        overlayY: 'top',
+        offsetY: 0,
+      },
+    ],
+  },
+  {
+    side: 'left',
+    align: 'bottom',
+    positions: [
+      {
+        originX: 'start',
+        originY: 'bottom',
+        overlayX: 'end',
+        overlayY: 'bottom',
+        offsetY: 0,
+      },
+    ],
+  },
+  {
+    side: 'bottom',
+    align: 'left',
+    positions: [
+      {
+        originX: 'start',
+        originY: 'bottom',
+        overlayX: 'start',
+        overlayY: 'top',
+        offsetY: 0,
+      },
+    ],
+  },
+  {
+    side: 'bottom',
+    align: 'right',
+    positions: [
+      {
+        originX: 'end',
+        originY: 'bottom',
+        overlayX: 'end',
+        overlayY: 'top',
+        offsetY: 0,
+      },
+    ],
+  },
+];
 
 describe('MenuPositionService', () => {
   let service: MenuPositionService;
@@ -13,4 +89,22 @@ describe('MenuPositionService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should convert menu position', () => {
+    for (const pos of POSITIONS) {
+      const positions = service.convertMenuPositionToConnectedPositions([
+        pos.side as MenuPositionValue,
+        pos.align as MenuPositionValue,
+      ]);
+
+      expect(positions[0]).toEqual(
+        pos.positions[0],
+        `[${pos.side}, ${pos.align}] to connected positions`
+      );
+    }
+  });
+
+  xit('should #recalculateMenu()', () => {});
+
+  xit('should #setPosition()', () => {});
 });
