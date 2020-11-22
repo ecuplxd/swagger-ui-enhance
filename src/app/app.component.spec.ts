73449a7361d2ff629ddeda5aee59fc1df354e32e
ecuplxd <12050901053@ecupl.edu.cn>
2020-11-22 19:12:14 +0800

    chore(test): AppModule

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,17 +1,32 @@
 import { APP_BASE_HREF } from '@angular/common';
 import { NO_ERRORS_SCHEMA } from '@angular/compiler';
-import { TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  flush,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
+import { StoreServiceStub } from 'src/__test__';
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
 import { StoreService } from './share/service';
+import { Any } from './share/share.model';
 
 describe('AppComponent', () => {
+  let app: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let store: StoreServiceStub;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       schemas: [NO_ERRORS_SCHEMA],
       imports: [AppModule],
       providers: [
-        StoreService,
+        {
+          provide: StoreService,
+          useClass: StoreServiceStub,
+        },
         {
           provide: APP_BASE_HREF,
           useValue: '/',
@@ -21,9 +36,24 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+  it('should create the app', fakeAsync(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+    store = TestBed.inject(StoreService) as StoreServiceStub;
+    fixture.detectChanges();
+
+    tick(600);
+
     expect(app).toBeTruthy();
+
+    flush();
+  }));
+
+  it('should parse drop files', () => {
+    spyOn(store, 'parseFile').and.callThrough();
+    const files = [1] as Any;
+    app.parseFiles(files);
+
+    expect(store.parseFile).toHaveBeenCalled();
   });
 });
